f08345e14af497d29131f3def18a696c133083fd
martyncolmer <martyc1974@hotmail.com>
2020-01-20 14:36:21 +0000

    added import fo getQuestionnaireSummary

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,11 @@
 const getQuestionnaireFromRegistry = require("./getQuestionnaireFromRegistry");
 const getQuestionnaireFromPublisher = require("./getQuestionnaireFromPublisher");
 const insertIntoSurveyRegistry = require("./insertIntoSurveyRegistry");
+const getQuestionnaireSummary = require("./getQuestionnaireSummary");
 
 module.exports = {
   getQuestionnaireFromRegistry,
   getQuestionnaireFromPublisher,
-  insertIntoSurveyRegistry
+  insertIntoSurveyRegistry,
+  getQuestionnaireSummary
 };
