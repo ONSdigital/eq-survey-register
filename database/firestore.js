65ba4a54d11ffcc5f6f6d5271fa65928384187c6
martyncolmer <martyc1974@hotmail.com>
2020-01-23 12:01:30 +0000

    Added support for Google Auth

--- database/firestore.js
+++ database/firestore.js
@@ -1,9 +1,9 @@
 const {Firestore} = require('@google-cloud/firestore');
 
 let db;
-if (process.env.NODE_ENV.toUpperCase() == "DEVELOPMENT"){
+if(process.env.GOOGLE_AUTH_PROJECT_ID){
   db = new Firestore({
-    projectId: process.env.FIRESTORE_PROJECT_ID
+    projectId: process.env.GOOGLE_AUTH_PROJECT_ID
     //keyFilename: '../test_firestore_account.json'
   })
 }
