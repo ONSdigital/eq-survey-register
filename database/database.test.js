414f186922554fc9ba2cd4c7754f6974aebdcfcd
martyncolmer <martyc1974@hotmail.com>
2020-01-24 16:42:22 +0000

    Updated to fix questionnaire ordering in JSON

--- database/database.test.js
+++ database/database.test.js
@@ -6,7 +6,7 @@ const mockModel = () => {
     survey_id: "123",
     form_type: "456",
     survey_version: "1",
-    schema: { eq_id: "123" },
+    schema: JSON.stringify({ eq_id: "123", version: "1", title: "test" }),
     title: "test",
     language: "en",
     runner_version: "v1"
@@ -27,11 +27,11 @@ describe.each(databases)("testing database modules", (databaseName) => {
     jest.resetModules()
     process.env.DATABASE = databaseName
     database = require(".")
-  })
-
-  beforeEach(() => {
     model = mockModel()
     req = mockRequest()
+    await database.saveQuestionnaire(model)
+    model.schema = JSON.stringify({ eq_id: "123", version: "2", title: "test" })
+    await database.saveQuestionnaire(model)
   })
 
   it(`should save a schema into the database using ${databaseName}`, async () => {
@@ -41,25 +41,19 @@ describe.each(databases)("testing database modules", (databaseName) => {
   })
 
   it(`should retrieve the latest version using ${databaseName}`, async () => {
-    await database.saveQuestionnaire(model)
     data = await database.getQuestionnaire(req)
-    model.sort_key = "0"
-    expect(data).toMatchObject(model)
-    expect(data).toMatchObject({ sort_key: "0", registry_version: "1" })
+    expect(data).toMatchObject({ eq_id: "123", title: "test" })
+    expect(data).toMatchObject({ version: "2" })
   })
 
   it(`should retrieve a specific version using ${databaseName}`, async () => {
     req.version = "1"
-    model.sort_key = "1"
     data = await database.getQuestionnaire(req)
-    expect(data).toMatchObject(model)
-    expect(data).toMatchObject({ sort_key: "1", registry_version: "1" })
+    expect(data).toMatchObject({ eq_id: "123", title: "test" })
+    expect(data).toMatchObject({ version: "1" })
   })
 
   it(`should retieve a list of latest schema version using ${databaseName}`, async () => {
-    await database.saveQuestionnaire(model)
-    model.survey_id = "456"
-    await database.saveQuestionnaire(model)
     data = await database.getQuestionnaireSummary(true)
     expect(data).toEqual(expect.arrayContaining([expect.objectContaining({ sort_key: "0" })]))
     expect(data).toEqual(expect.not.arrayContaining([expect.objectContaining({ sort_key: "1" })]))
