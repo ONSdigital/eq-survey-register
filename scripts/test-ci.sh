cea5f6ee733edd70bf5eb54ac4014823ab407f2d
martyncolmer <martyc1974@hotmail.com>
2020-01-26 15:11:31 +0000

    tweak to docker network

--- scripts/test-ci.sh
+++ scripts/test-ci.sh
@@ -6,8 +6,8 @@ AWS_ACCESS_KEY_ID=dummy \
 AWS_SECRET_ACCESS_KEY=dummy \
 NODE_ENV=development \
 PUBLISHER_URL=ttp://localhost:9000/publish/ \
-DYNAMODB_ENDPOINT_OVERRIDE=http://host.docker.internal:8000 \
-FIRESTORE_EMULATOR_HOST=host.docker.internal:8080 \
+DYNAMODB_ENDPOINT_OVERRIDE=http://dynamo:8000 \
+FIRESTORE_EMULATOR_HOST=firestore:8080 \
 FIRESTORE_PROJECT_ID=test-project \
 yarn jest --runInBand --detectOpenHandles --forceExit "$@"
 
